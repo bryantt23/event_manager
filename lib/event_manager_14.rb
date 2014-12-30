1adb9e910b6ec49243836a145e2b5f174867e758
bryant tee <bryantt23@gmail.com>
2014-12-30 01:05:57 -0800

    fin

--- lib/event_manager_14.rb
+++ lib/event_manager_14.rb
@@ -4,54 +4,7 @@
 
 require 'csv'
 require 'sunlight/congress'
-
-Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
-
-def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5,"0")[0..4]
-end
-
-puts "EventManager initialized."
-
-contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
-
-=begin
-contents.each do |row|
-  name = row[:first_name]
-
-  zipcode = clean_zipcode(row[:zipcode])
-
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-
-  puts "#{name} #{zipcode} #{legislators}"
-end
-=end
-
-=begin
-
-contents.each do |row|
-  name = row[:first_name]
-
-  zipcode = clean_zipcode(row[:zipcode])
-
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
-
-  legislators_string = legislator_names.join(", ")
-
-  puts "#{name} #{zipcode} #{legislators_string}"
-end
-=end
-
-
-
-# Moving Displaying Legislators to a Method
-
-require 'csv'
-require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -60,30 +13,33 @@ def clean_zipcode(zipcode)
 end
 
 def legislators_by_zipcode(zipcode)
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
 
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
+def save_thank_you_letters(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
   end
-
-  legislator_names.join(", ")
 end
 
 puts "EventManager initialized."
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
 
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letters(id,form_letter)
 end
-
-
-
-
-
